f488df14891192fdb0a01d91ab17ab06ccf63011
Arthur_Tougma <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-05-20 12:38:00 +0200

    entity and DAO for : User, Exercice

---
+++ app/src/main/java/mi1/todolist/db/ExerciceDAO.java
@@ -0,0 +1,31 @@
+package mi1.todolist.db;
+
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+
+    @Query("SELECT * FROM user")
+    List<Task> getAll();
+
+    @Insert
+    void insert(User user);
+
+    @Insert
+    long[] insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+
+    @Update
+    void update(User user);
+
+}
