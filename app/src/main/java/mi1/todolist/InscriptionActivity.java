c289dec0df5e7be48ea9ce12ee2d481db8e1147d
TinyTheRealCoder <benj.trebor@gmail.com>
2022-05-23 14:54:08 +0200

    Strings

--- app/src/main/java/mi1/todolist/InscriptionActivity.java
+++ app/src/main/java/mi1/todolist/InscriptionActivity.java
@@ -1,6 +1,22 @@
 package mi1.todolist;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import mi1.todolist.db.DatabaseClient;
+
 public class InscriptionActivity extends AppCompatActivity {
+
+    // DATA
+    private DatabaseClient mDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Récupération du DatabaseClient
+        mDb = DatabaseClient.getInstance(getApplicationContext());
+    }
 }
