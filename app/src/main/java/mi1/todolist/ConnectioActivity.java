7079346d75bb215fd1ca5bd5c8f80823c801316a
Arthur_Tougma <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-05-23 14:18:06 +0200

    appcompact on activity

--- app/src/main/java/mi1/todolist/ConnectioActivity.java
+++ app/src/main/java/mi1/todolist/ConnectioActivity.java
@@ -2,5 +2,5 @@ package mi1.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InscriptionActivity extends AppCompatActivity {
+public class ConnectioActivity extends AppCompatActivity {
 }
