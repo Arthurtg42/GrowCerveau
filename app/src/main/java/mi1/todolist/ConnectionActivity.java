c9d93e49d72bab95db4f1655f72b2fa304fd0cdc
TinyTheRealCoder <benj.trebor@gmail.com>
2022-05-31 09:23:41 +0200

    Inscription page done

--- app/src/main/java/mi1/todolist/ConnectionActivity.java
+++ app/src/main/java/mi1/todolist/ConnectionActivity.java
@@ -1,12 +1,17 @@
 package mi1.todolist;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import mi1.todolist.db.DatabaseClient;
+import mi1.todolist.db.User;
 
 public class ConnectionActivity extends AppCompatActivity {
 
@@ -15,6 +20,10 @@ public class ConnectionActivity extends AppCompatActivity {
 
     // DATA
     private DatabaseClient mDb;
+
+    private EditText Pseudo;
+    private EditText Mdp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +36,68 @@ public class ConnectionActivity extends AppCompatActivity {
 
     public void ConnectionActivityConnexion(View view){
 
-        if(){
+        Pseudo = findViewById(R.id.ConnectionActivity_pseudo);
+        Mdp = findViewById(R.id.ConnectionActivity_mdp);
+
+        class CheckConnection extends AsyncTask<Void, Void, Integer> {
+
+            @Override
+            protected Integer doInBackground(Void... voids) {
+
+                // adding to database
+
+                Log.d("QUERY", "GOOOOOOOOOOOOOOOOOOOOOD4");
+                if(Pseudo.getText().toString().trim()+"" == ""){
+                    Toast.makeText(getApplicationContext(), "Veuillez remplir le Pseudo", Toast.LENGTH_LONG).show();
+                }
+                else if(Mdp.getText().toString().trim()+"" == ""){
+                    Toast.makeText(getApplicationContext(), "Veuillez remplir le Mot de passe", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    return mDb.getAppDatabase().userDao().getLog(Pseudo.getText().toString(), Mdp.getText().toString());
+                }
+                Log.d("QUERY", "GOOOOOOOOOOOOOOOOOOOOOD3");
+                return 0;
+
+            }
+
+            @Override
+            protected void onPostExecute(Integer idUser) {
+                super.onPostExecute(idUser);
+
+                // Quand la tache est créée, on arrête l'activité AddTaskActivity (on l'enleve de la pile d'activités)
+                setResult(idUser);
+                finish();
+
+                Log.d("QUERY", "GOOOOOOOOOOOOOOOOOOOOOD2");
+            }
+        }
+
+        //On execute
+        CheckConnection st = new CheckConnection();
+        st.execute();
+        /*
+        st.get
+
+        if(idUser != 0){
             //La connection est réussie
 
             ////////////
             // On charge la home page
             // Création d'une intention
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, HomePageActivity.class);
             // Lancement de la demande de changement d'activité
             startActivityForResult(intent, REQUEST_CODE_ADD);
+            Log.d("QUERY", "GOOOOOOOOOOOOOOOOOOOOOD");
         }
         else{
             //La connection a échoué
+            Toast.makeText(getApplicationContext(), "Mot de passe ou pseudo incorrect", Toast.LENGTH_LONG).show();
         }
 
+         */
+
     }
+
 
 }
--- app/src/main/java/mi1/todolist/db/UserDAO.java
+++ app/src/main/java/mi1/todolist/db/UserDAO.java
@@ -28,7 +28,7 @@ public interface UserDAO {
     @Update
     void update(User user);
 
-    //@Query("SELECT ID FROM user WHERE mdp = motDePasseLog AND pseudo = pseudoLog")
-    //public User getLog(String pseudoLog, String motDePasseLog);
+    @Query("SELECT ID FROM user WHERE mdp = :motDePasseLog AND pseudo = :pseudoLog")
+    int getLog(String pseudoLog, String motDePasseLog);
 
 }
